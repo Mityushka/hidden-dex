afd796670dc357ec07d3d87f5a406836e4411d5f
Mitya <mityaoreh@gmail.com>
2020-06-01 18:51:42 +0300

    refactor: change structure of objects and reworks the function structure
    resolves: #5

--- public/js/collection.js
+++ public/js/collection.js
@@ -1,5 +1,5 @@
 const elctrodeExHiddenLegends = {
-  picLink: "../img/cards/ex5/ex5_5.png",
+  picLink: "ex5/ex5_5.png",
   cardType: "Pokemon",
   name: "Electrode",
   type: ["lightning"],
@@ -7,24 +7,30 @@ const elctrodeExHiddenLegends = {
   HP: 70,
   illustrator: "Kyoko Umemoto",
   set: "Hidden Legends",
-  firstAttack: {
-    attackCost: ["lightning"],
-    attackText:
-      "Swift 30 - This attack's damage isn't affected by Weakness, Resistance, Poké-Powers, Poké-Bodies, or any other effects on the Defending Pokémon.",
-  },
-  secondAttack: {
-    attackCost: ["colorless", "colorless", "colorless"],
-    attackText:
-      "Mass Destruction - Both Electrode and the Defending Pokémon are now Knocked Out. If Electrode has any Special Energy cards attached to it, this attack does nothing.",
-  },
+  attack: [
+    {
+      cost: ["lightning"],
+      name: "Swift",
+      power: "30",
+      text:
+        "This attack's damage isn't affected by Weakness, Resistance, Poké-Powers, Poké-Bodies, or any other effects on the Defending Pokémon.",
+    },
+    {
+      cost: ["colorless", "colorless", "colorless"],
+      name: "Mass Destruction",
+      power: null,
+      text:
+        "Both Electrode and the Defending Pokémon are now Knocked Out. If Electrode has any Special Energy cards attached to it, this attack does nothing.",
+    },
+  ],
   weakness: ["fighting"],
-  resistence: ["noResistence"],
+  resistence: [],
   retreatCost: ["colorless"],
-  setNumber: ["5 / 101"],
+  setNumber: 5,
   rare: "Holo Rare",
 };
 const ninetalesExtraExHiddenLegends = {
-  picLink: "../img/cards/ex5/ex5_96.png",
+  picLink: "ex5/ex5_96.png",
   cardType: "Pokemon",
   name: "Ninetales ex",
   type: ["fire"],
@@ -32,24 +38,29 @@ const ninetalesExtraExHiddenLegends = {
   HP: 90,
   illustrator: "Ryo Ueda",
   set: "Hidden Legends",
-  firstAttack: {
-    attackCost: ["colorless"],
-    attackText:
-      "Intense Glare - Switch 1 of your opponent's Benched Pokémon with 1 of the Defending Pokémon. Your opponent chooses the Defending Pokémon to switch. The new Defending Pokémon is now Burned and Confused.",
-  },
-  secondAttack: {
-    attackCost: ["fire", "colorless", "colorless", "colorless"],
-    attackText:
-      "Fire Blast 100 - Discard a Fire Energy attached to Ninetales ex.",
-  },
+  attack: [
+    {
+      cost: ["colorless"],
+      name: "Intense Glare",
+      power: null,
+      text:
+        "Switch 1 of your opponent's Benched Pokémon with 1 of the Defending Pokémon. Your opponent chooses the Defending Pokémon to switch. The new Defending Pokémon is now Burned and Confused.",
+    },
+    {
+      cost: ["fire", "colorless", "colorless", "colorless"],
+      name: "Fire Blast",
+      power: 100,
+      text: "Discard a Fire Energy attached to Ninetales ex.",
+    },
+  ],
   weakness: ["water"],
-  resistence: ["noResistence"],
+  resistence: [],
   retreatCost: ["colorless"],
-  setNumber: ["96/ 101"],
+  setNumber: 96,
   rare: "Rare Holo EX",
 };
 const pinsirExHiddenLegends = {
-  picLink: "../img/cards/ex5/ex5_13.png",
+  picLink: "ex5/ex5_13.png",
   cardType: "Pokemon",
   name: "Pinsir",
   type: ["grass"],
@@ -57,21 +68,30 @@ const pinsirExHiddenLegends = {
   HP: 70,
   illustrator: "Hajime Kusajima",
   set: "Hidden Legends",
-  ability:
-    "Crust - Any damage done to Pinsir by attacks from your opponent's Basic Pokémon is reduced by 30 (after applying Weakness and Resistance).",
-  firstAttack: {
-    attackCost: ["grass", "colorless"],
-    attackText:
-      "Sonicboom 30 - This attack's damage isn't affected by Weakness or Resistance.",
-  },
+  setID: "EX5",
+  abilities: [
+    {
+      name: "Crust",
+      text:
+        "Any damage done to Pinsir by attacks from your opponent's Basic Pokémon is reduced by 30 (after applying Weakness and Resistance).",
+    },
+  ],
+  attacks: [
+    {
+      cost: ["grass", "colorless"],
+      name: "Sonicboom",
+      power: 30,
+      text: "This attack's damage isn't affected by Weakness or Resistance.",
+    },
+  ],
   weakness: ["fire"],
-  resistence: ["noResistence"],
+  resistence: [],
   retreatCost: ["colorless"],
-  setNumber: ["13 / 101"],
+  setNumber: 13,
   rare: "HoloRare",
 };
 const voltrobExHiddenLegends = {
-  picLink: "../img/cards/ex5/ex5_90.png",
+  picLink: "ex5/ex5_90.png",
   cardType: "Pokemon",
   name: "Voltrob",
   type: ["lightning"],
@@ -79,23 +99,29 @@ const voltrobExHiddenLegends = {
   HP: 40,
   illustrator: "Kyoko Umemoto",
   set: "Hidden Legends",
-  firstAttack: {
-    attackCost: ["colorless"],
-    attackText:
-      "Recharge - Search your deck for a Lightning Energy card and attach it to Voltorb. Shuffle your deck afterward.",
-  },
-  secondAttack: {
-    attackCost: ["lightning", "colorless"],
-    attackText: "Rolling Attack 20",
-  },
+  attack: [
+    {
+      cost: ["colorless"],
+      name: "Recharge",
+      power: null,
+      text:
+        "Search your deck for a Lightning Energy card and attach it to Voltorb. Shuffle your deck afterward.",
+    },
+    {
+      cost: ["lightning", "colorless"],
+      name: "Rolling Attack",
+      power: 20,
+      text: null,
+    },
+  ],
   weakness: ["fighting"],
-  resistence: ["noResistence"],
+  resistence: [],
   retreatCost: ["colorless"],
-  setNumber: ["80 / 101"],
+  setNumber: 80,
   rare: "Common",
 };
 const vulpixExHiddenLegends = {
-  picLink: "../img/cards/ex5/ex5_81.png",
+  picLink: "ex5/ex5_81.png",
   cardType: "Pokemon",
   name: "Vulpix",
   type: ["fire"],
@@ -103,20 +129,29 @@ const vulpixExHiddenLegends = {
   HP: 50,
   illustrator: "Toshinao Aoki",
   set: "Hidden Legends",
-  firstAttack: { attackCost: ["colorless"], attackText: "Scratch 10" },
-  secondAttack: {
-    attackCost: ["fire"],
-    attackText:
-      "Ascension - Search your deck for a card that evolves from Vulpix and put it on Vulpix. (This counts as evolving Vulpix.) Shuffle your deck afterward.",
-  },
+  attack: [
+    {
+      cost: ["colorless"],
+      name: "Scratch",
+      power: 10,
+      text: null,
+    },
+    {
+      cost: ["fire"],
+      name: "Ascension",
+      power: null,
+      text:
+        "Search your deck for a card that evolves from Vulpix and put it on Vulpix. (This counts as evolving Vulpix.) Shuffle your deck afterward.",
+    },
+  ],
   weakness: ["water"],
-  resistence: ["noResistence"],
+  resistence: [],
   retreatCost: ["colorless"],
-  setNumber: ["81 / 101"],
+  setNumber: 81,
   rare: "Common",
 };
 const jirachiExHiddenLegends = {
-  picLink: "../img/cards/ex5/ex5_8.png",
+  picLink: "ex5/ex5_8.png",
   cardType: "Pokemon",
   name: "Jirachi",
   type: ["psychic", "steel"],
@@ -124,20 +159,25 @@ const jirachiExHiddenLegends = {
   HP: 70,
   illustrator: "Ryo Ueda",
   set: "Hidden Legends",
-  firstAttack: {
-    attackCost: ["colorless"],
-    attackText:
-      "Make a Wish - Search your deck for a card that evolves from 1 of your Pokémon and put it on that Pokémon. (This counts as evolving that Pokémon.) If you do, put 1 damage counter on Jirachi. Shuffle your deck afterward.",
-  },
-  secondAttack: {
-    attackCost: ["psychic", "steel"],
-    attackText:
-      "Mind Bend 30 - Flip a coin. If heads, the Defending Pokémon is now Confused.",
-  },
+  attack: [
+    {
+      cost: ["colorless"],
+      name: "Make a Wish",
+      power: null,
+      text:
+        "Search your deck for a card that evolves from 1 of your Pokémon and put it on that Pokémon. (This counts as evolving that Pokémon.) If you do, put 1 damage counter on Jirachi. Shuffle your deck afterward.",
+    },
+    {
+      cost: ["psychic", "steel"],
+      name: "Mind Bend",
+      power: 30,
+      text: "Flip a coin. If heads, the Defending Pokémon is now Confused.",
+    },
+  ],
   weakness: ["fire"],
-  resistence: ["noResistence"],
+  resistence: [],
   retreatCost: ["colorless"],
-  setNumber: ["8 / 101"],
+  setNumber: 8,
   rare: "HoloRare",
 };
 
@@ -149,25 +189,23 @@ const collection = [
   elctrodeExHiddenLegends,
   jirachiExHiddenLegends,
 ];
-const sortByName = (arr) => {
-  arr.sort((a, b) => {
-    if (a.name < b.name) return -1;
-    if (a.name > b.name) return 1;
-    return 0;
-  });
+const sortByPokmeonName = (a, b) => {
+  return a.name.localeCompare(b.name);
 };
+const sortArray = (array, fn) => {
+  const newArray = [...array];
+  newArray.sort(fn);
+  return newArray;
+};
+sortArray(collection, sortByPokmeonName);
 
-const addOl2Html = (pokemons) => {
-  sortByName(pokemons);
-  const list = document.querySelector("body");
+const renderListOfPokemon = (pokemons) => {
   const orderedList = document.createElement("ol");
-  orderedList.classList.add("pokeList");
-  list.appendChild(orderedList);
-  const temp = document.querySelector(".pokeList");
   for (let i = 0; i < pokemons.length; i++) {
     const listItem = document.createElement("li");
     listItem.textContent = pokemons[i].name;
-    temp.appendChild(listItem);
+    orderedList.appendChild(listItem);
   }
+  return orderedList;
 };
-addOl2Html(collection);
+document.body.appendChild(renderListOfPokemon(collection));
